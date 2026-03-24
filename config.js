83cda02f3b6e2784701de4d35586c1e02c97a00f
ancroedition-ship-it <ancroedition@gmail.com>
2026-03-24 09:46:05 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-// config.js
 const GROQ_CONFIG = {
-    API_KEY: "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", // Replace with your gsk_ key
+    API_KEY: "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", 
     MODEL: "llama-3.3-70b-versatile"
 };
