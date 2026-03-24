8587b7075b0ba9a9acb62f54eae4cd48427601c2
ancroedition-ship-it <ancroedition@gmail.com>
2026-03-24 11:46:57 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const GROQ_CONFIG = {
     API_KEY: "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", 
-    MODEL: "deepseek-r1-distill-llama-70b"
+    MODEL: "llama-3.2-11b-vision-preview"
 };
