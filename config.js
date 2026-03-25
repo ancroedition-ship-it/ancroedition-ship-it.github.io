06ab1c368159384352730a71a88de480d63c2f3b
ancroedition-ship-it <ancroedition@gmail.com>
2026-03-25 17:53:21 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,3 @@
 const GROQ_CONFIG = {
     API_KEY: "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",
-    MODEL: "llama3-8b-8192", // You can also use 'llama3-70b-8192' or 'mixtral-8x7b-32768'
-    MAX_TOKENS: 1024,
-    TEMPERATURE: 0.7
-};
-
-export default GROQ_CONFIG;
+    MODEL: "llama3-8b-8192",
