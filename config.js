0ba60c6ea2060a36457a97f13d8be66e3e38f0ff
ancroedition-ship-it <ancroedition@gmail.com>
2026-03-25 17:58:30 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
-const GROQ_CONFIG = {
-    API_KEY: "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",
+// Global configuration object
+const NEURO_CONFIG = {
+    API_KEY: ""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", 
     MODEL: "llama-3.3-70b-versatile",
+    PROXY: "https://api.allorigins.win/raw?url="
+};
