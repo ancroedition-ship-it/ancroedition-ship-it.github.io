c200030a385b0c9ae30eab273909355a3fac951d
ancroedition-ship-it <ancroedition@gmail.com>
2026-03-25 08:24:12 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // Neurosentix Studio - API Configuration
-const GROQ_CONFIG = {
-    API_KEY: "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",
-    MODEL: "llama-3.3-70b-versatile"
-};
+//const GROQ_CONFIG = {
+    //API_KEY: "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",
+    //MODEL: "llama-3.3-70b-versatile"
+//};
 
 const CLAUDE_CONFIG = {
     API_KEY: "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",
